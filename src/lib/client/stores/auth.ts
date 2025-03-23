70be8d6dc1f03ffd64cb6203f89e4f9de58e86ec
larwis95 <156850505+larwis95@users.noreply.github.com>
2025-03-23 02:46:50 -0400

    chore(authentication documentation): added documentation for the authentication API

--- src/lib/client/stores/auth.ts
+++ src/lib/client/stores/auth.ts
@@ -18,9 +18,36 @@ export type AuthStore = {
 	) => () => void;
 };
 
+/**
+ * Authentication store to manage user authentication, keeps track of the user's login status, as well as the currently logged in user
+ * @returns {AuthStore}
+ * @property {Writable<boolean>} isLoggedIn - The user's login status
+ * @property {() => Promise<void>} checkAuth - Function to check if the user is authenticated
+ * @property {(username: string, password: string) => Promise<void>} login - Function to log in the user
+ * @property {(username: string, password: string) => Promise<void>} register - Function to register the user
+ * @property {() => Promise<void>} logout - Function to log out the user
+ * @property {Writable<null | User>} user - The currently logged in user
+ * @property {(run: (value: null | User) => void, invalidate?: (value?: null | User) => void) => () => void} subscribe - Function to subscribe to the user store
+ *
+ * @example
+ * <script>
+ * import { auth } from '$lib/client/stores/auth';
+ * import { onMount } from 'svelte';
+ *  const handleLogin = async () => {
+ * 		await auth.login('username', 'password');
+ * 	};
+ * </script>
+ * <button on:click={handleLogin}>Login</button>
+ * <p>{$auth ? $auth.username : 'Not logged in'}</p>
+ */
 export function createAuthStore(): AuthStore {
 	const isLoggedIn = writable(false);
 	const user = writable(null);
+
+	/**
+	 * Function to check if the user is authenticated
+	 * @returns {Promise<void>}
+	 */
 
 	const checkAuth = async () => {
 		const response = await fetch('/api/users/me', {
@@ -37,6 +64,12 @@ export function createAuthStore(): AuthStore {
 		}
 	};
 
+	/**
+	 * Function to log in the user
+	 * @param {string} username - The username of the user
+	 * @param {string} password - The password of the user
+	 * @returns {Promise<void>}
+	 */
 	const login = async (username: string, password: string) => {
 		const response = await fetch('/api/auth/login', {
 			method: 'POST',
@@ -56,6 +89,10 @@ export function createAuthStore(): AuthStore {
 		}
 	};
 
+	/**
+	 * Function to log out the user
+	 * @returns {Promise
+	 */
 	const logout = async () => {
 		const response = await fetch('/api/auth/logout', {
 			method: 'POST'
@@ -67,6 +104,12 @@ export function createAuthStore(): AuthStore {
 		}
 	};
 
+	/**
+	 * Function to register the user
+	 * @param {string} username - The username of the user
+	 * @param {string} password - The password of the user
+	 * @returns {Promise<void>}
+	 */
 	const register = async (username: string, password: string) => {
 		const response = await fetch('/api/users', {
 			method: 'POST',
